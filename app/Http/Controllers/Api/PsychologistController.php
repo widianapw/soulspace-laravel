e40be21ca4c846ab0c94420abf7acba02a020865
Widiana Putra <widianaputraa@gmail.com>
2024-09-28 21:19:02 +0700

    fix nearest location

--- app/Http/Controllers/Api/PsychologistController.php
+++ app/Http/Controllers/Api/PsychologistController.php
@@ -12,12 +12,21 @@ class PsychologistController extends Controller
     public function get(Request $request)
     {
         if($request->has("latitude") && $request->has("longitude")) {
-//            sort by distance
-            $psychologists = Psychologist::select("name", "phone_number", "address", "latitude", "longitude")
-                ->havingRaw("SQRT(POW(69.1 * (latitude - ?), 2) + POW(69.1 * (? - longitude) * COS(latitude / 57.3), 2)) < 5", [$request->latitude, $request->longitude])
+            $latitude = $request->latitude;
+            $longitude = $request->longitude;
+
+            // Using Haversine formula to calculate distance
+            $psychologists = Psychologist::select("*")
+                ->selectRaw(
+                    "( 3959 * acos( cos( radians(?) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(?) ) + sin( radians(?) ) * sin( radians( latitude ) ) ) ) AS distance",
+                    [$latitude, $longitude, $latitude]
+                )
+//                ->having("distance", "<", 5) // Distance less than 5 miles (or kilometers, depending on your choice)
+                ->orderBy("distance")
                 ->get();
+
             return PsychologistResource::collection($psychologists);
-        }else {
+        } else {
             $psychologists = Psychologist::get();
             return PsychologistResource::collection($psychologists);
         }
